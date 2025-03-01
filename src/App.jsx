b37d9854415d34eaa6762b5fcd7243a99c41cb14
SamMidnight03 <ssgomezeliz@gmail.com>
2025-02-28 22:25:20 -0500

    tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
-import './App.css'
-import Header from './Components/Header'
-import Explorar from './Pages/Explorar'
-import Registro from './Pages/Registro'
-import Login from './Pages/Login'
+import '/App.css'
+import Header from '/Components/Header'
+import Explorar from '/Pages/Explorar'
+import Registro from '/Pages/Registro'
+import Login from '/Pages/Login'
 
 function App() {
 
